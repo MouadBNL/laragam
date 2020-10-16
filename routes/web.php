49321037f01cab678560373193ea4f57cb705039
Mouad Benali <mouadbnl.2k01@gmail.com>
2020-10-16 19:06:29 +0100

    renaming welcome to home and applying the auth middleware

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group([
+    'middleware' => 'auth'
+],function ()
+{
+    Route::view('/', 'home');
 });
